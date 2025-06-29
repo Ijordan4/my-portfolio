d40cb7bd96c11c979419af844c4e6c0002afe0cc
Isaiah Jordan <IsaiahJordan200@gmail.com>
2025-06-29 00:48:17 -0400

    starter site

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'; // ✅ Add this line
 import App from './App';
-
-// Note: No CSS import here because style.css is linked in index.html
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/Ijordan4/my-portfolio">
+  <BrowserRouter basename="/my-portfolio">   {/* ✅ Corrected basename */}
     <App />
   </BrowserRouter>
 );
